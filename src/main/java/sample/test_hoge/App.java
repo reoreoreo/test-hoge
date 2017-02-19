c5845d5dbf51a6d449ac4b246be139962e108bb0
reo <devloprk555@gmail.com>
2017-02-19 23:59:10 +0900

    test commit

---
+++ src/main/java/sample/test_hoge/App.java
@@ -0,0 +1,13 @@
+package sample.test_hoge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
