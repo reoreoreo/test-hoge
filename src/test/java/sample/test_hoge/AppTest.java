8a5e1a79460baa3869f6da36869118228791c199
reo <devloprk555@gmail.com>
2017-02-20 23:34:54 +0900

    test commit

--- src/test/java/sample/test_hoge/AppTest.java
+++ src/test/java/sample/test_hoge/AppTest.java
@@ -1,38 +1,32 @@
 package sample.test_hoge;
 
-import junit.framework.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+        String baseUrl = "https://www.google.co.jp/";
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testCase() throws Exception {
+        Thread.sleep(13000);
+
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
     }
 }
