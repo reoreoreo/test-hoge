1a8e19e3fb59e62a4d2371341252f897889dcfda
reo <devloprk555@gmail.com>
2017-03-13 23:06:20 +0900

    test commit

--- src/test/java/sample/test_hoge/runCucumberTest.java
+++ src/test/java/sample/test_hoge/runCucumberTest.java
@@ -2,8 +2,10 @@ package sample.test_hoge;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(strict = false, format = { "pretty", "json:target/cucumber.json" })
 public class runCucumberTest {
 }
