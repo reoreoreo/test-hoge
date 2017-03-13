69efaca143a248e0d9ff8a36cf01e9a7570a5431
reo <devloprk555@gmail.com>
2017-03-13 22:19:28 +0900

    test commit

--- src/test/java/sample/test_hoge/SampleWebDriverTest.java
+++ src/test/java/sample/test_hoge/SampleWebDriverTest.java
@@ -4,13 +4,14 @@ import java.net.URL;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import junit.framework.TestCase;
-
+@Ignore
 public class SampleWebDriverTest extends TestCase {
 
     WebDriver driver;
